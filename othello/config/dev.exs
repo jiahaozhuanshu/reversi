168fd55bb0fd75c60cecec285d12bdc994388362
vipul018 <vipulsharma018@gmail.com>
2018-03-06 18:49:44 -0500

    DB update

--- othello/config/dev.exs
+++ othello/config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :othello, Othello.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "othello",
+  password: "ye4Ahsh5thee",
   database: "othello_dev",
   hostname: "localhost",
   pool_size: 10
